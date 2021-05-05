d7442a170af174110c32bb357a0cbb87a491b57d
Amit Chaudhari <amitchaudhari9121@gmail.com>
2021-05-05 15:28:36 +0530

    update

---
+++ ap.py
@@ -0,0 +1,9 @@
+from newspaper import Article
+
+# url = 'https://www.eurekalert.org/pub_releases/2021-04/fm-fhb042921.php'
+
+# article = Article(url)
+# article.download()
+# article.parse()
+text = """https://medium.com/@mr_mig_by/growing-as-software-engineer-6ee490ad58db"""
+print("data="+text)
--- app.py
+++ app.py
@@ -6,6 +6,13 @@ import torch
 import json 
 from transformers import T5Tokenizer, T5ForConditionalGeneration, T5Config
 ############
+from newspaper import Article
+
+
+#TODO: This text generation is slow. I want to train my own model!
+# - Tokenizer sequences longer than 512 dont seem to work well.
+# - create a custom tokenizing divider. it divides that huge ass article into smaller ,500~ish sequence length
+# - 
 
 app = Flask(__name__)
 api = Api(app)
@@ -15,7 +22,7 @@ config = {
     "CACHE_TYPE": "SimpleCache",  # Flask-Caching related configs
     "CACHE_DEFAULT_TIMEOUT": 2000 # time out is 2 seconds. yeah, I'll try to decrease it later.
 }
-cache = Cache(config=config)      # this is for the decorator.
+# cache = Cache(config=config)      # this is for the decorator.
 
 text = {}
 
@@ -24,38 +31,44 @@ tokenizer = T5Tokenizer.from_pretrained('t5-small')
 device = torch.device('cpu')
 
 # @cache.cached(timeout=3000)
-def summarize_text_def(text, max_length=0.5, earlyStop=False):
-    #TODO: add input support for more Model genereation control.
-    #Add num beams control.
-    # Add minimum length support, 
-    # Add maximum length support,
-    # Add limits to all the above functions.
-    # Add a feature where you bypass and just output the string as is 
-    # If it is less than some number of words.
+def summarize_text_def(text, max_length=0.5, early_stop=False):
     preprocess_text = text.strip().replace("\n"," ")
+    preprocess_text = preprocess_text.strip().replace("\t","  ")
+
     t5_prepared_Text = "summarize: "+preprocess_text
     tokenized_text = tokenizer.encode(t5_prepared_Text, return_tensors="pt").to(device)
     summary_ids = model.generate(tokenized_text,
                                     num_beams=2,
                                     no_repeat_ngram_size=3,
-                                    min_length=int(len(text)/2), #refactor this later
-                                    max_length=len(text),
-                                    early_stopping=earlyStop)
+                                    min_length=60, #refactor this later
+                                    max_length=150,
+                                    early_stopping=early_stop)
     text_summarized = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
     return text_summarized
 
 class summarize_text(Resource):
-    def get(self,text_id):
-        #TODO: save them inside some proper db I guess?
-        text[text_id] = request.form['data']
-        return {text_id: summarize_text_def(text[text_id])}
+    def get(self):
+        text = request.form['data']
+        if (text.count(" ") < 1000):
+            return {"Summary": summarize_text_def(text)}
+        else: return {"Text is too long, Please understand that this is an experiment. Help me in & Buy me a Coffee for bigger limits."}
 
-    def put(self,text_id):
-        return {text_id: text[text_id]}
-api.add_resource(summarize_text, '/<string:text_id>')
+class parse_article_links(Resource):
+    def get(self):
+        url = request.form['data']
+        article = Article(url)
+        article.download()
+        article.parse()
+        text = article.text
+        return summarize_text_def(text)
 
-class summarize_article_links(Resource):
-    pass
+class parse_reddit_posts(Resource):
+    def get(self):
+        pass
+
+api.add_resource(summarize_text, '/api')
+api.add_resource(parse_article_links, '/api/link')
+api.add_resource(parse_reddit_posts,'/api/reddit-post')
 
 if __name__ == '__main__':
     app.config.from_mapping(config)
